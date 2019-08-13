3add475bb321771f3685861b9dc52363cf7bcc52
doori30 <rublju0070@naver.com>
2019-08-13 11:58:40 +0900

    pageMaker

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,9 @@
+//삼항연산자
+//조건?"참일 때":"거짓일때"
+
+console.log(3>5?"참":"거짓");
+
+//data-set
 var html = '';
 var site = "https://webmir.co.kr/score";
 var scoreURL = {
@@ -7,10 +13,10 @@ var scoreURL = {
 	dURL : site + "/score_del.php",	
 }
 
-getList(5);
+getList(1);
 function getList(page){
 	$.ajax({
-		type: "get",
+		type: "get",//통신방법..get&post
 		url: scoreURL.rURL, //URL은 대문자로 쓸 것!
 		data:{
 			page:page
@@ -18,6 +24,7 @@ function getList(page){
 		dataType: "json",
 		success: function (res) {
 			console.log(res);
+			$(".score-tb").find("tbody").empty();
 			for(var i in res.student){
 				html = '<tr>';
 				html += '<th>'+res.student[i].stdname+'</th>';
@@ -39,27 +46,66 @@ function getList(page){
 	});
 }
 function pagerMaker(total,page){
-	var cnt = Math.ceil(total/10); //소수점 올림.
+	var cnt = Math.ceil(total/10); //소수점 올림.page에 따라 한페이지에 10개씩 보임. 전체 페이지 갯수
+	var div = 3;                  //세트당 나올 페이지 수
+	var stn = 0;                   //세트중에 시작페이지
+	var edn = 0;                   //세트중에 마지막페이지
+	var prev = 0;                  //"<" 클릭시 나타날 페이지
+	var next = 0;                  //">"클릭시 나타날 페이지
+	var prevShow = false;          // true 면 활성화 false면 click이 안됨.비 활성화
+	var lastShow = false;          // true 면 활성화 false면 click이 안됨.비 활성화
+	var first = 1;                 //"<<" 클릭시 나타날 페이지
+	var last = cnt;                  //">>"클릭시 나타날 페이지
+	var nowIndex =(Math.ceil(page/div)-1);   // 페이지 세트의 현재 index
+	var lastIndex =(Math.ceil(cnt/div)-1);   // 페이지 세트의 마지막 index
 
-html = '<li class="page-item page-prev">';
+	/* temp = Math.floor(cnt/div);       */      //2/3=0.....에서 소수점을 버림. 결론은 0
+	stn = nowIndex * div + 1;     //소수점을 반올림해서 올림 보여질 숫자를 찾아냄. 세트의 시작 페이지 값
+	if(cnt < stn + div -1) edn = cnt;
+	else edn = stn + div - 1;
+
+	if(nowIndex > 0){
+prevShow = true;
+prev = stn-1;
+	}
+
+	if(lastIndex > nowIndex){
+		lastShow = true;
+		next = edn+1;
+	}
+
+	console.log("stn:"+stn);
+	console.log("edn:"+edn);
+	console.log("lastIndex:"+lastIndex);
+	console.log("nowIndex:"+nowIndex);
+	//prevShow?"disabled":""
+	//          true    / false
+
+	html = '<li class="page-item page-first'+(prevShow?"":"disabled")+'" data-page="'+first+'">';
 html += '<span class="page-link"><i class="fas fa-angle-double-left"></i></span>';
 html += '</li>';
-html += '<li class="page-item page-lt">';
+html += '<li class="page-item page-prev'+(prevShow?"":"disabled")+'" data-page="'+prev+'">';
 html += '<span class="page-link"><i class="fas fa-angle-left"></i></span>';
 html += '</li>';
-for(var i=1; i<=cnt; i++){
-	html += '<li class="page-item page-ct">';
+for(var i=stn; i<=edn; i++){
+	html += '<li class="page-item page-ct '+(page==i?"active":"")+'" data-page="'+i+'">';
 html += '<span class="page-link">'+i+'</span>';
 html += '</li>';
 }
-html += '<li class="page-item page-rt">';
+html += '<li class="page-item page-next '+(prevShow?"":"disabled")+'" data-page="'+next+'">';
 html += '<span class="page-link"><i class="fas fa-angle-right"></i></span>';
 html += '</li>';
-html += '<li class="page-item page-next">';
+html += '<li class="page-item page-last'+(prevShow?"":"disabled")+'" data-page="'+last+'">';
 html += '<span class="page-link"><i class="fas fa-angle-double-right"></i></span>';
 html += '</li>';
-$(".pager").html(html);
-if(page == 1) $(".page-lt").addClass("disabled");
+ $(".pager").html(html);
+ $(".page-item").click(function(){
+	 if(!$(this).hasClass("disabled"))
+	 //pager-item이 disabled를 가지고 있어=true클릭이 되기 때문에 fals로 만들어서 클릭이 되지 않게 만들어줌.
+	 //!-not. true=>fals로 fals=>true로 만들어줌.
+	 getList( $(this).data("page") );
+ });
+/*if(page == 1) $(".page-lt").addClass("disabled");
 if(page == cnt) $(".page-rt").addClass("disabled");
-$(".page-ct").eq(page-1).addClass("active");
+$(".page-ct").eq(page-1).addClass("active"); */
 }
